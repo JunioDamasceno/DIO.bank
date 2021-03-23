d1cd5cd1346580cd42a535fc6a827360cb3e7eaa
Junio da Silva Damasceno <juniowin@yahoo.com.br>
2021-03-22 22:56:32 -0300

    Projeto finalizado

--- DIO.bank/Program.cs
+++ DIO.bank/Program.cs
@@ -11,31 +11,31 @@ namespace DIO.bank
         {
             string opcaoUsuario = ObterOpcaoUsuario();
 
-            while(opcaoUsuario.ToUpper() != "x")
+            while(opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                    ListarContas();
+                        ListarContas();
                         break;
                     case "2":
-                    InserirConta();
+                        InserirConta();
                         break;
                     case "3":
-                    //Transferir();
+                    Transferir();
                         break;
                     case "4":
-                   //Sacar();
+                        Sacar();
                         break;
                     case "5":
-                    //Depositar();
+                       Depositar();
                        break;
                     case "C":
-                    Console.Clear();
+                        Console.Clear();
                         break;
 
                     default:
-                    throw new ArgumentOutOfRangeException();
+                        throw new ArgumentOutOfRangeException("Você não utilizou uma opção válida. ");
                 }
 
                 opcaoUsuario = ObterOpcaoUsuario();
@@ -43,6 +43,44 @@ namespace DIO.bank
             }
 
             Console.WriteLine("Obrigado por utilizar nossos serviços!");
+            Console.ReadLine();
+
+        }
+
+        private static void Transferir()
+        {
+            Console.Write("Digite o número da conta de origem: ");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número da conta destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser transferido: ");
+            double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+        }
+
+        private static void Depositar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Depositar(valorDeposito);
+        }
+
+        private static void Sacar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorSaque);
 
         }
 
@@ -63,7 +101,7 @@ namespace DIO.bank
             for (int i = 0; i < listContas.Count; i++)
             {
                 Conta conta = listContas[i];
-                Console.Write("#(0) - ", i);
+                Console.Write("#{0} - ", i);
                 Console.WriteLine(conta);
 
             }
